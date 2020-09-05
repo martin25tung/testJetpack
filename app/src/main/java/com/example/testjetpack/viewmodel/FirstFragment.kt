65592cbe4e791f828220b568395e4cb89ef1ef97
martin25tung <martin25tung@gmail.com>
2020-09-05 22:06:47 +0800

    connect first and second fragment

--- app/src/main/java/com/example/testjetpack/viewmodel/FirstFragment.kt
+++ app/src/main/java/com/example/testjetpack/viewmodel/FirstFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.testjetpack.R
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_first.*
 
 
 /**
@@ -20,5 +22,14 @@ class FirstFragment : Fragment() {
 //        return super.onCreateView(inflater, container, savedInstanceState)
         val view = layoutInflater.inflate(R.layout.fragment_first, container, false)
         return view
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        button1.setOnClickListener {
+            findNavController().navigate(R.id.action_firstFragment_to_blankFragment2)
+        }
+
     }
 }
