838b0608108c77f820e3380a3bf16018f0234197
martin25tung <martin25tung@gmail.com>
2020-09-05 21:43:23 +0800

    add Navigation graphic

---
+++ app/src/main/java/com/example/testjetpack/viewmodel/BlankFragment2.kt
@@ -0,0 +1,20 @@
+package com.example.testjetpack.viewmodel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [BlankFragment2.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class BlankFragment2 : Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+}
---
+++ app/src/main/java/com/example/testjetpack/viewmodel/FirstFragment.kt
@@ -0,0 +1,21 @@
+package com.example.testjetpack.viewmodel
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [FirstFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class FirstFragment : Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
