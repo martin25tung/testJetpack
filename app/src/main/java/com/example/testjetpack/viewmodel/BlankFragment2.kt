0afeae1c33a295dbed86cf6f633a18d6328d4794
martin25tung <martin25tung@gmail.com>
2020-09-05 23:23:34 +0800

    在fragment2顯示資料並加上空安全

--- app/src/main/java/com/example/testjetpack/viewmodel/BlankFragment2.kt
+++ app/src/main/java/com/example/testjetpack/viewmodel/BlankFragment2.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.example.testjetpack.R
 import kotlinx.android.synthetic.main.fragment_blank2.*
 
@@ -17,13 +18,21 @@ import kotlinx.android.synthetic.main.fragment_blank2.*
  */
 class BlankFragment2 : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    val args: BlankFragment2Args by navArgs()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return layoutInflater.inflate(R.layout.fragment_blank2, container, false)
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        button2.setOnClickListener{
+        tv_name.text = args.person?.name
+        tv_age.text = args.person?.age.toString()
+        button2.setOnClickListener {
             findNavController().navigate(R.id.action_blankFragment2_to_firstFragment)
         }
     }
--- app/src/main/java/com/example/testjetpack/viewmodel/FirstFragment.kt
+++ app/src/main/java/com/example/testjetpack/viewmodel/FirstFragment.kt
@@ -32,8 +32,9 @@ class FirstFragment : Fragment() {
             // 無參數單存轉換畫面
 //            findNavController().navigate(R.id.action_firstFragment_to_blankFragment2)
 
+
             // 新的參數型態model 需要打包(Parcelable)或序列化(Seralizable)
-            val person = Person(input_name.toString(), input_age.toString().toInt())
+            val person = Person(input_name.text.toString(), input_age.text.toString().toIntOrNull()?:0)
             // 包含參數的目的地傳遞
             val action =
                 FirstFragmentDirections.actionFirstFragmentToBlankFragment2(person)
